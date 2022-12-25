ed0ef89b5a423cbea34c3b4dd02466bffd520ff2
Shunta Nakamura <113566477+k21rs094@users.noreply.github.com>
2022-12-26 04:23:27 +0900

    使っているのはToFではなく、フォトリフレクタなので変数名を適切な名前に変更した。
    本プログラムは、M5STACK勉強会で得たプログラムをフォトリフレクタ用に改良したものである。

--- Shunta/photo_reflector.ino
+++ Shunta/photo_reflector.ino
@@ -1,20 +1,21 @@
 // M5Stamp-C3のプログラム
 // このプログラムの構造を保ったままモーターや距離センサー用にアレンジできる。
 // 今回はフォトリフレクタ用にアレンジした。これは、モノの検知に用いるものである。
-#define ToF 4 // フォトリフレクタ
-#define LED 5 // LED
+#define PHOTO_SENSOR 4 // フォトリフレクタ用のピン番号
+#define LED 5 // LED用のピン番号
 
 void setup() {
-  pinMode(ToF, INPUT);  // ToF番ピンを入力に設定
-  pinMode(LED, OUTPUT); // LED番ピンを出力に設定
+  pinMode(PHOTO_SENSOR, INPUT);  // PHOTO_SENSOR番のピンを入力に設定
+  pinMode(LED, OUTPUT); // LED番のピンを出力に設定
   Serial.begin(115200); // シリアル通信の設定
 }
 
 void loop() {
-  int data = analogRead(ToF); // フォトリフレクタの取得値を読み込む
-  Serial.println(data);       // シリアルモニタに取得値を出力
+  int dish_object_distance = analogRead(PHOTO_SENSOR); // フォトリフレクタの取得値を読み込み、変数dish_object_distanceに代入
+  Serial.println(dish_object_distance); // フォトリフレクタの取得値をシリアルモニタに出力
+
   // ここから、皿にモノが置かれたのか検知する処理を記述
-  if (data > 1000){
+  if (dish_object_distance > 1000){
     digitalWrite(LED, LOW);
   } else {
     digitalWrite(LED, HIGH);
