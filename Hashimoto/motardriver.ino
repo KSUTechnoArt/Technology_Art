1be6dbd469944d7235b09af8dcb75b142703ff12
Shunta Nakamura <113566477+k21rs094@users.noreply.github.com>
2022-12-26 04:33:18 +0900

    プログラムの読みやすさを改善（処理内容はそのまま）

--- Hashimoto/motardriver.ino
+++ Hashimoto/motardriver.ino
@@ -1,15 +1,11 @@
-
 #include <Wire.h> 
 
 const int motorR = 0x60;
 const int motorL = 0x68;
 long Speed;
-
 long SpeedL, SpeedR;
 
-
-void setup()
-{
+void setup() {
   delay(1000);
   //Wire1.begin(25, 21);
   Wire.begin(1, 0);
@@ -23,25 +19,12 @@ void setup()
   delay(1000);
 }
 
-
-//モータドライバ I2C制御 motor driver I2C
-void writeMotorResister(int motor, byte vset, byte data1){
-  int vdata = vset << 2 | data1;
-  Wire.beginTransmission(motor);
-  Wire.write(0x00);
-  Wire.write(vdata);
-  Wire.endTransmission(true);
-}
-
-void loop()
-{
-
-  byte i;
-
-  while(1){
-    for(i=0x06;i<=0x3F;i++){
-      writeMotorResister(motorR, i, 0x01);
-      writeMotorResister(motorL, i, 0x01);
+void loop() {
+  byte car_speed;
+  while(1) {
+    for(car_speed = 0x06; car_speed <= 0x3F; car_speed++){
+      writeMotorResister(motorR, car_speed, 0x01);
+      writeMotorResister(motorL, car_speed, 0x01);
       delay(200);
     }
   
@@ -49,9 +32,9 @@ void loop()
     writeMotorResister(motorL, 0x00, 0x00); //停止
     delay(4000);
     
-    for(i=0x06;i<=0x3F;i++){
-      writeMotorResister(motorR, i, 0x02);
-      writeMotorResister(motorL, i, 0x02);
+    for(car_speed = 0x06; car_speed <= 0x3F; car_speed++){
+      writeMotorResister(motorR, car_speed, 0x02);
+      writeMotorResister(motorL, car_speed, 0x02);
       delay(200);
     }
   
@@ -59,6 +42,13 @@ void loop()
     writeMotorResister(motorL, 0x00, 0x00); //停止
     delay(4000);
   }
+}
 
-  
+//モータドライバ I2C制御 motor driver I2C
+void writeMotorResister(int motor, byte vset, byte data1) {
+  int vdata = vset << 2 | data1;
+  Wire.beginTransmission(motor);
+  Wire.write(0x00);
+  Wire.write(vdata);
+  Wire.endTransmission(true);
 }
