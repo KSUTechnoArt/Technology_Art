512f7d389b7eea11437a7224fcd810420c059d40
Shunta Nakamura <113566477+k21rs094@users.noreply.github.com>
2022-12-26 04:58:40 +0900

    プログラムの移植がしやすいようにインデントの調整をした。

--- Sadakata/MTOF171000C0.ino
+++ Sadakata/MTOF171000C0.ino
@@ -8,28 +8,22 @@
 //21  SCL
 
 #include <Wire.h>
-
 #define ADDRESS 0x52
 
 uint16_t distance;
 uint16_t distance_tmp;
 uint8_t data_cnt;
 
-void setup() 
-{
+void setup() {
   Serial.begin(115200);
   Serial.println("MTOF171000C0_I2C");
-  //I2Cの初期化
-  Wire.begin(1 , 0);  //SDA:G1, SCL:G0
-
+  // I2Cの初期化
+  Wire.begin(1, 0);  // SDA:G1, SCL:G0
   delay(1000);
 }
 
-void loop() 
-{
-
+void loop() {
   Serial.print("distance = ");
-
   Wire.beginTransmission(ADDRESS);
   Wire.write(0xD3);
   Wire.endTransmission(false);
@@ -37,20 +31,17 @@ void loop()
   data_cnt = 0;
   distance = 0;
   distance_tmp = 0;
-  while(Wire.available())
-  {
+  while(Wire.available()) {
     distance_tmp = Wire.read();
-    distance = (distance << (data_cnt*8)) | distance_tmp;
+    distance = (distance << (data_cnt * 8)) | distance_tmp;
     data_cnt++;
   }
-
   Serial.print(distance);
   Serial.println(" mm");
   delay(45);
 }
 
-int readDistance(byte reg)
-{
+int readDistance(byte reg) {
   Wire.beginTransmission(ADDRESS);
   Wire.write(reg);
   Wire.endTransmission(false);
@@ -58,10 +49,9 @@ int readDistance(byte reg)
   data_cnt = 0;
   distance = 0;
   distance_tmp = 0;
-  while(Wire.available())
-  {
+  while(Wire.available()) {
     distance_tmp = Wire.read();
-    distance = (distance << (data_cnt*8)) | distance_tmp;
+    distance = (distance << (data_cnt * 8)) | distance_tmp;
     data_cnt++;
   }
   return distance;
