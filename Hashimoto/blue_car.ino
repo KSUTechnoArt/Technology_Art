8489ed6ba219d2e09fafd08eef68cde1beb0a760
Shunta Nakamura <113566477+k21rs094@users.noreply.github.com>
2022-12-26 04:48:36 +0900

    プログラムの読みやすさを改善（処理内容はそのまま）。また、プログラムの先頭部分に説明文を追加した。

--- Hashimoto/blue_car.ino
+++ Hashimoto/blue_car.ino
@@ -1,3 +1,6 @@
+// 橋本くんのモーターに関するプログラムと貞方先生が用意したフォトリフレクタに関するプログラムを融合したもの。
+// 車から5cm以内に障害物があれば、0.5秒間停止するプログラムである。
+
 //Sharp MTOF171000C0 I2C
 //Due MTOF
 //3.3 VDD
@@ -20,15 +23,15 @@ uint8_t data_cnt;
 void setup() {
   delay(1000);
   //Wire1.begin(25, 21);
-  Wire.begin(1, 0);  //SDA:G1, SCL:G0
-  writeMotorResister(motorR, 0x00, 0x00); //停止
-  writeMotorResister(motorL, 0x00, 0x00); //停止
-  writeMotorResister(motorR, 0x01, 0x80); //エラー解除
-  writeMotorResister(motorL, 0x01, 0x80); //エラー解除
+  Wire.begin(1, 0);  // SDA:G1, SCL:G0
+  writeMotorResister(motorR, 0x00, 0x00); // 停止
+  writeMotorResister(motorL, 0x00, 0x00); // 停止
+  writeMotorResister(motorR, 0x01, 0x80); // エラー解除
+  writeMotorResister(motorL, 0x01, 0x80); // エラー解除
   delay(1000);
   Serial.begin(115200);
   Serial.println("MTOF171000C0_I2C");
-  //I2Cの初期化
+  // I2Cの初期化
   delay(1000);
 }
 
@@ -44,36 +47,36 @@ void loop() {
     distance_tmp = 0;
     while(Wire.available()) {
       distance_tmp = Wire.read();
-      distance = (distance << (data_cnt*8)) | distance_tmp;
+      distance = (distance << (data_cnt * 8)) | distance_tmp;
       data_cnt++;
     }
     Serial.print(distance);
     Serial.println(" mm");
-    byte carSpeed;
-    for (carSpeed = 0x09; carSpeed <= 0x10; carSpeed++) {
-      if (carSpeed == 0x1E) {
-        carSpeed--;
+    byte car_speed;
+    for (car_speed = 0x09; car_speed <= 0x10; car_speed++) {
+      if (car_speed == 0x1E) {
+        car_speed--;
       }
-      writeMotorResister(motorR, carSpeed, 0x02);
-      writeMotorResister(motorL, carSpeed, 0x02);
+      writeMotorResister(motorR, car_speed, 0x02);
+      writeMotorResister(motorL, car_speed, 0x02);
       //delay(200);
     }
-    //for(carSpeed = 0x06; carSpeed <= 0x3F; carSpeed++){
-     // writeMotorResister(motorR, carSpeed, 0x02);
-     // writeMotorResister(motorL, carSpeed, 0x02);
-      //delay(200);
+    //for(car_speed = 0x06; car_speed <= 0x3F; car_speed++) {
+    //    writeMotorResister(motorR, car_speed, 0x02);
+    //    writeMotorResister(motorL, car_speed, 0x02);
+    //    delay(200);
     //}
-    if(distance <= 50){
-        writeMotorResister(motorR, 0x00, 0x00); //停止
-        writeMotorResister(motorL, 0x00, 0x00); //停止
-        delay(500);
+    if(distance <= 50) {
+      writeMotorResister(motorR, 0x00, 0x00); // 停止
+      writeMotorResister(motorL, 0x00, 0x00); // 停止
+      delay(500);
     }
   }
   delay(45);
 }
 
-//モータドライバ I2C制御 motor driver I2C
-void writeMotorResister(int motor, byte vset, byte data1){
+// モータドライバ I2C制御 motor driver I2C
+void writeMotorResister(int motor, byte vset, byte data1) {
   int vdata = vset << 2 | data1;
   Wire.beginTransmission(motor);
   Wire.write(0x00);
@@ -91,7 +94,7 @@ int readDistance(byte reg) {
   distance_tmp = 0;
   while(Wire.available()) {
     distance_tmp = Wire.read();
-    distance = (distance << (data_cnt*8)) | distance_tmp;
+    distance = (distance << (data_cnt * 8)) | distance_tmp;
     data_cnt++;
   }
   return distance;
