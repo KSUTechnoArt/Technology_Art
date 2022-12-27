2cc5b73d41837412a036b42578ab82653fb1117e
Shunta Nakamura <113566477+k21rs094@users.noreply.github.com>
2022-12-27 13:53:58 +0900

    モーターとToFセンサーとフォトリフレクタのプログラムを組み合わせた

--- Hashimoto/blue_car.ino
+++ Hashimoto/blue_car.ino
@@ -1,5 +1,5 @@
-// 橋本くんのモーターに関するプログラムと貞方先生が用意したToFに関するプログラムを融合したもの。
-// 車から5cm以内に障害物があれば、0.5秒間停止するプログラムである。
+// モーターとToFセンサーとフォトリフレクタのプログラムを組み合わせたもの。
+// 「車から5cm以内に障害物がある」または「お皿にモノが置かれていない」ならば、0.5秒間停止するプログラムである。
 
 //Sharp MTOF171000C0 I2C
 //Due MTOF
@@ -11,15 +11,21 @@
 //21  SCL
 
 #include <Wire.h>
+// 定数:モーター関係
 const int motorR = 0x60;
 const int motorL = 0x68;
 long Speed;
 long SpeedL, SpeedR;
 #define ADDRESS 0x52
 
+// 定数:ToFセンサー関係
 uint16_t distance;
 uint16_t distance_tmp;
 uint8_t data_cnt;
+
+// 定数:フォトリフレクタ関係
+#define PHOTO_SENSOR 4 // フォトリフレクタ用のピン番号
+#define LED 5 // LED用のピン番号 <開発の最終段階で削除する>
 
 void setup() {
   delay(1000);
@@ -30,48 +36,51 @@ void setup() {
   writeMotorResister(motorR, 0x01, 0x80); // エラー解除
   writeMotorResister(motorL, 0x01, 0x80); // エラー解除
   delay(1000);
-  Serial.begin(115200);
-  Serial.println("MTOF171000C0_I2C");
-  // I2Cの初期化
+  pinMode(PHOTO_SENSOR, INPUT);  // PHOTO_SENSOR番のピンを入力に設定
+  pinMode(LED, OUTPUT); // LED番のピンを出力に設定
+  Serial.begin(115200); // シリアル通信の設定}
   delay(1000);
 }
 
 void loop() {
-  while(1) {
-    Serial.print("distance = ");
-    Wire.beginTransmission(ADDRESS);
-    Wire.write(0xD3);
-    Wire.endTransmission(false);
-    Wire.requestFrom(ADDRESS, 2);
-    data_cnt = 0;
-    distance = 0;
-    distance_tmp = 0;
-    while(Wire.available()) {
-      distance_tmp = Wire.read();
-      distance = (distance << (data_cnt * 8)) | distance_tmp;
-      data_cnt++;
+  readDistance(0xD3); // 距離の読み込み(ToFセンサー)
+  Serial.print("distance = ");
+  Serial.print(distance); // ToFセンサーの取得値をシリアルモニタに出力
+  Serial.println(" mm");
+
+  int dish_object_distance = analogRead(PHOTO_SENSOR); // フォトリフレクタの取得値を読み込み、変数dish_object_distanceに代入
+  Serial.print("Photo Sensor:");
+  Serial.println(dish_object_distance); // フォトリフレクタの取得値をシリアルモニタに出力
+
+  byte car_speed;
+  for(car_speed = 0x09; car_speed <= 0x10; car_speed++) {
+    if(car_speed == 0x1E) {
+      car_speed--;
     }
-    Serial.print(distance);
-    Serial.println(" mm");
+    writeMotorResister(motorR, car_speed, 0x02);
+    writeMotorResister(motorL, car_speed, 0x02);
+    //delay(200);
+  }
+  //for(car_speed = 0x06; car_speed <= 0x3F; car_speed++) {
+  //    writeMotorResister(motorR, car_speed, 0x02);
+  //    writeMotorResister(motorL, car_speed, 0x02);
+  //    delay(200);
+  //}
+  if(distance <= 50 || dish_object_distance > 1000) {
+    digitalWrite(LED, LOW);
+    writeMotorResister(motorR, 0x00, 0x00); // 停止
+    writeMotorResister(motorL, 0x00, 0x00); // 停止
+    delay(500);
+  } else {
+    digitalWrite(LED, HIGH);
     byte car_speed;
     for (car_speed = 0x09; car_speed <= 0x10; car_speed++) {
       if (car_speed == 0x1E) {
         car_speed--;
       }
+    }
       writeMotorResister(motorR, car_speed, 0x02);
       writeMotorResister(motorL, car_speed, 0x02);
-      //delay(200);
-    }
-    //for(car_speed = 0x06; car_speed <= 0x3F; car_speed++) {
-    //    writeMotorResister(motorR, car_speed, 0x02);
-    //    writeMotorResister(motorL, car_speed, 0x02);
-    //    delay(200);
-    //}
-    if(distance <= 50) {
-      writeMotorResister(motorR, 0x00, 0x00); // 停止
-      writeMotorResister(motorL, 0x00, 0x00); // 停止
-      delay(500);
-    }
   }
   delay(45);
 }
@@ -85,6 +94,7 @@ void writeMotorResister(int motor, byte vset, byte data1) {
   Wire.endTransmission(true);
 }
 
+// 距離の読み込み(ToFセンサー)
 int readDistance(byte reg) {
   Wire.beginTransmission(ADDRESS);
   Wire.write(reg);
