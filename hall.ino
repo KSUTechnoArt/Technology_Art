e9963573939b96ef924c2087067dff84f28406a8
Shunta Nakamura <113566477+k21rs094@users.noreply.github.com>
2023-01-02 13:14:26 +0900

    ピン番号と変数名を変更した

--- hall.ino
+++ hall.ino
@@ -1,8 +1,8 @@
 // 定数:ホールセンサー関係
-#define LEFT_HALL_SENSOR 4 // ホールセンサー用のピン番号
-#define RIGHT_HALL_SENSOR 1 // ホールセンサー用のピン番号
-int distance_Left_Hall;
-int distance_Right_Hall;
+#define LEFT_HALL_SENSOR 1 // ホールセンサー用のピン番号
+#define RIGHT_HALL_SENSOR 0 // ホールセンサー用のピン番号
+int value_Left_Hall;
+int value_Right_Hall;
 
 void setup() {
   pinMode(LEFT_HALL_SENSOR, INPUT); // LEFT_HALL_SENSOR番のピンを入力に設定
@@ -11,11 +11,11 @@ void setup() {
 }
 
 void loop() {
-  distance_Left_Hall = analogRead(LEFT_HALL_SENSOR); // ホールセンサーの取得値を読み込み、変数distance_Left_Hallに代入
-  distance_Right_Hall = analogRead(RIGHT_HALL_SENSOR); // ホールセンサーの取得値を読み込み、変数distance_Right_Hallに代入
-  Serial.print("distance_Left_Hall = ");
-  Serial.print(distance_Left_Hall); // ホールセンサーの取得値をシリアルモニタに出力
-  Serial.print("\t distance_Right_Hall = ");
-  Serial.println(distance_Right_Hall); // ホールセンサーの取得値をシリアルモニタに出力
+  value_Left_Hall = analogRead(LEFT_HALL_SENSOR); // ホールセンサーの取得値を読み込み、変数value_Left_Hallに代入
+  value_Right_Hall = analogRead(RIGHT_HALL_SENSOR); // ホールセンサーの取得値を読み込み、変数value_Right_Hallに代入
+  Serial.print("value_Left_Hall = ");
+  Serial.print(value_Left_Hall); // ホールセンサーの取得値をシリアルモニタに出力
+  Serial.print("\t value_Right_Hall = ");
+  Serial.println(value_Right_Hall); // ホールセンサーの取得値をシリアルモニタに出力
   delay(100);
 }
